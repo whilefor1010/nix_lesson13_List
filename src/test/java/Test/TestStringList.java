1ab7697c82402accab813728f3b8b8ebd93570dd
whilefor1010 <whilefor1010@gmail.com>
2022-11-22 17:16:25 +0200

    first commit

---
+++ src/test/java/Test/TestStringList.java
@@ -0,0 +1,13 @@
+package Test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestStringList {
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
